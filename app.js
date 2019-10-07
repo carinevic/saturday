b1fe493cb9902d21381b0ed513d71d1e8b977465
Carine Ngwekwe <carine@Carines-MacBook-Pro.local>
2019-10-06 22:19:57 -0500

    database connected

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+let mongoose = require('mongoose');
 const app = express()
+let Post = require('./models/post').Post;
 
+mongoose.connect('mongodb://localhost/city', { useNewUrlParser: true });
 
+app.get('/post',async (req,res) =>{
+    let posts = await Post.find();
+    res.send(posts);
+})
 
 app.use(express.static('public'));
 app.listen(3000,()=>{
